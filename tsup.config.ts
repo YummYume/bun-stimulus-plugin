bd36e2190ff606bb23fe8fad6276432c04577f94
YummYume <yummyume@users.noreply.github.com>
2024-03-04 00:11:37 +0100

    Fix type generation and build

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  dts: {
+    banner: '/// <reference path="../types/import-definition.d.ts" />',
+  },
 });
